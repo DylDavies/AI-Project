b8b22b3f242ea3f5bc9141b89cc4944f13e4840a
Dylan Davies <34412943+DylDavies@users.noreply.github.com>
2026-05-09 14:17:31 +0200

    part 2 sub 2

---
+++ part_2/all_next.py
@@ -0,0 +1,35 @@
+from chess import Board, Move
+from reconchess import utilities
+
+FEN = input()
+
+def get_moves(board: Board) -> list[str]:
+    moves: list[str] = list()
+
+    for move in board.generate_pseudo_legal_moves():
+        moves.append(move.uci())
+
+    for move in utilities.without_opponent_pieces(board).generate_castling_moves():
+        if not utilities.is_illegal_castle(board, move):
+            moves.append(move.uci())
+
+    moves.append("0000")
+
+    return list(dict.fromkeys(sorted(moves)))
+
+def make_move(board: Board, move: Move) -> str:
+    board.push(move)
+
+    return board.fen()
+
+def get_all_boards_fromm_next_move(initial_board: Board) -> list[str]:
+    boards: list[str] = list()
+
+    for move in get_moves(initial_board):
+        board = Board(FEN)
+        boards.append(make_move(board, Move.from_uci(move)))
+
+    return list(dict.fromkeys(sorted(boards)))
+
+for board in get_all_boards_fromm_next_move(Board(FEN)):
+    print(board)
